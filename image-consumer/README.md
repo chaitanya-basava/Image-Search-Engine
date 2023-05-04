5666ca4be39948cb11c0cbae9a54533c3b2f017e
chaitanya-basava <chaitanya.basava@gmail.com>
2023-05-05 01:04:53 +0530

    clip image model spark udf

---
+++ image-consumer/README.md
@@ -0,0 +1,6 @@
+# Image consumer [WIP]
+
+run the spark application using following command (may update)
+```
+spark-submit --master "local[3]" --deploy-mode client clip_model.py
+```
--- image-consumer/clip_model.py
+++ image-consumer/clip_model.py
@@ -1,23 +1,50 @@
+import torch
+import mlflow
 import requests
+import pandas as pd
 from PIL import Image
 from transformers import logging
-from transformers import AutoProcessor, CLIPModel, AutoTokenizer
+from transformers import AutoProcessor, CLIPModel
+
+from pyspark.sql import SparkSession
+from pyspark.sql.types import DoubleType, ArrayType
 
 logging.set_verbosity(40)
 
-model_name = "openai/clip-vit-base-patch32"
-model = CLIPModel.from_pretrained(model_name)
-processor = AutoProcessor.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-url = "https://farm66.staticflickr.com/65535/52743059408_a9eac98298_z.jpg"
-image = Image.open(requests.get(url, stream=True).raw)
-inputs = processor(images=image, return_tensors="pt")
+class ClipImageEmbeddingModel(mlflow.pyfunc.PythonModel):
+    def __init__(self, model_name="openai/clip-vit-base-patch32"):
+        self.model_name = model_name
+        self.device = torch.cuda if torch.cuda.is_available() else torch.cpu
+        self.model = CLIPModel.from_pretrained(self.model_name)
+        self.processor = AutoProcessor.from_pretrained(self.model_name)
 
-image_features = model.get_image_features(**inputs)
-print(image_features.shape)
+    def predict(self, context, image_url):
+        image = Image.open(requests.get(image_url, stream=True).raw)
+        inputs = self.processor(images=image, return_tensors="pt")
+        image_features = self.model.get_image_features(**inputs)
 
-inputs = tokenizer(["a photo of a cat"], padding=True, return_tensors="pt")
+        return pd.Series(image_features.squeeze(0).cpu().detach().numpy())
 
-text_features = model.get_text_features(**inputs)
-print(text_features.shape)
+
+# clipImageEmbeddingModel = ClipImageEmbeddingModel()
+# mlflow.pyfunc.save_model(path="./mlflow_clip_model", python_model=clipImageEmbeddingModel)
+
+# url = "https://farm66.staticflickr.com/65535/52743059408_a9eac98298_z.jpg"
+# loaded_model = mlflow.pyfunc.load_model("./mlflow_clip_model")
+# print(loaded_model.predict(url))
+
+spark = SparkSession \
+    .builder \
+    .master("local[3]") \
+    .appName("Image Consumer App") \
+    .config("spark.port.maxRetries", 100) \
+    .getOrCreate()
+
+clip_model = mlflow.pyfunc.spark_udf(
+    spark,
+    model_uri="./mlflow_clip_model",
+    result_type=ArrayType(DoubleType()),
+    env_manager="conda"
+)
+print(clip_model)
