2b19317d8aaafcf965fcbfe7e93025b9ea7a943d
chaitanya-basava <chaitanya.basava@gmail.com>
2023-05-06 01:21:11 +0530

    confluent avro msg deserialization fix

--- image-consumer/main.py
+++ image-consumer/main.py
@@ -1,7 +1,6 @@
-import json
 from pyspark.sql import SparkSession
 from pyspark.sql.avro.functions import from_avro
-from pyspark.sql.functions import col, regexp_replace
+from pyspark.sql.functions import expr
 
 from clip_model import load_model_udf
 
@@ -18,15 +17,15 @@ if __name__ == "__main__":
     predict = load_model_udf(spark)
     avroSchema = open("../schemas/flickr_image.avsc", "r").read()
 
-    df = spark.read \
-        .format("kafka") \
-        .option("kafka.bootstrap.servers", "localhost:9092") \
-        .option("subscribe", "flickr-images") \
-        .option("startingOffsets", "earliest") \
-        .load() \
+    df = (
+        spark.read
+        .format("kafka")
+        .option("kafka.bootstrap.servers", "localhost:9092")
+        .option("subscribe", "flickr-images")
+        .option("startingOffsets", "earliest")
+        .load()
+        .withColumn("value", expr("substring(value, 6, length(value)-5)"))  # needed when using confluent
         .withColumn("value", from_avro("value", avroSchema))
-
-    print(
-        df
-        .collect()
     )
+
+    df.select("value.*").show(truncate=False)
