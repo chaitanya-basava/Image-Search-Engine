f1b4d5b4349517665378fc72a825f9060bb1bd4f
chaitanya-basava <chaitanya.basava@gmail.com>
2023-05-29 13:32:30 +0530

    added image card component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,13 @@ import styles from './App.module.scss';
 import { Header } from './components/header/header';
 import Classnames from 'classnames';
 import Header_module from './components/header/header.module.scss';
+import { ImageCard } from './components/image-card/image-card';
 
 function App() {
     return (
         <div className={styles.App}>
             <Header />
+            {/* <ImageCard /> */}
         </div>
     );
 }
---
+++ client/src/_codux/boards/search-bar/search-bar.board.tsx
@@ -0,0 +1,7 @@
+import { createBoard } from '@wixc3/react-board';
+import { SearchBar } from '../../../components/search-bar/search-bar';
+
+export default createBoard({
+    name: 'SearchBar',
+    Board: () => <SearchBar />
+});
