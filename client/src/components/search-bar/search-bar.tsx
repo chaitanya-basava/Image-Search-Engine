55bffbd89a2e80abed8b053d24eb16272cf0c0f9
chaitanya-basava <chaitanya.basava@gmail.com>
2023-05-30 11:58:38 +0530

    implemented state management to display results

--- client/src/components/search-bar/search-bar.tsx
+++ client/src/components/search-bar/search-bar.tsx
@@ -42,20 +42,15 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     },
 }));
 
-function enterPressed(event: any) {
-    var code = event.keyCode || event.which;
-    var phrase = event.target.value;
-    if (code === 13 && phrase.length > 2) {
-        alert(phrase);
-        event.target.value = '';
-    }
+interface SearchBarProps {
+    enterPressed: Function;
 }
 
 /**
  * This component was created using Codux's Default new component template.
  * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
  */
-export const SearchBar = () => {
+export const SearchBar = ({ enterPressed }: SearchBarProps) => {
     return (
         <Search>
             <SearchIconWrapper>
