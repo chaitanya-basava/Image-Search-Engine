505a8f22a5d45b35bfc6bf764ec0ecf89724754e
chaitanya-basava <chaitanya.basava@gmail.com>
2023-05-30 20:16:18 +0530

    fixed file formatting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,77 +1,74 @@
 import React from 'react';
-        import { Header } from './components/header/header';
-        import { ResultsContainer } from './components/results-container/results-container';
-        import { ImageCardProps } from './components/image-card/image-card';
+import { Header } from './components/header/header';
+import { ResultsContainer } from './components/results-container/results-container';
+import { ImageCardProps } from './components/image-card/image-card';
 
-        interface AppState {
-        cards: Array
-<ImageCardProps>;
+interface AppState {
+    cards: Array<ImageCardProps>;
     phrase: string;
-    }
+}
 
-    class App extends React.Component<{}, AppState> {
+class App extends React.Component<{}, AppState> {
     constructor(props: {}) {
-    super(props);
-    this.state = {
-    cards: [],
-    phrase: "",
-    };
+        super(props);
+        this.state = {
+            cards: [],
+            phrase: '',
+        };
     }
 
     search = (event: any) => {
-    var code = event.keyCode || event.which;
-    var phrase = event.target.value;
-    if (code === 13 && phrase.length > 2) {
-    fetch('http://localhost:80/text_search', {
-    method: "POST",
-    mode: "cors",
-    body: JSON.stringify({
-    phrase: phrase
-    }),
-    headers: {
-    "Content-Type": "application/json",
-    }
-    })
-    .then(response => response.json())
-    .then(data => {
-    this.setState((state) => ({
-    cards: data.map(
-    (i: any) => {
-    return {
-    ...i._source,
-    score: i._score
-    }
-    }
-    ),
-    phrase: phrase
-    }));
-    })
-    .catch((err) => {
-    console.log(err.message);
-    });
+        var code = event.keyCode || event.which;
+        var phrase = event.target.value;
+        if (code === 13 && phrase.length > 2) {
+            fetch('http://localhost:80/text_search', {
+                method: 'POST',
+                mode: 'cors',
+                body: JSON.stringify({
+                    phrase: phrase,
+                }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    this.setState((state) => ({
+                        cards: data.map((i: any) => {
+                            return {
+                                ...i._source,
+                                score: i._score,
+                            };
+                        }),
+                        phrase: phrase,
+                    }));
+                })
+                .catch((err) => {
+                    console.log(err.message);
+                });
 
-    event.target.value = '';
-    }
+            event.target.value = '';
+        }
     };
 
     clearResults = () => {
-    this.setState((state) => ({
-    cards: [],
-    }));
+        this.setState((state) => ({
+            cards: [],
+        }));
     };
 
     render(): React.ReactNode {
-    return (
-    <div>
-        <Header
-                search={this.search}
-        clear={this.clearResults}
-        displayClear={this.state.cards.length !== 0}
-        />
-        <ResultsContainer cards={this.state.cards} phrase={this.state.phrase} />
-    </div>
-    );
+        return (
+            <div>
+                <Header
+                    search={this.search}
+                    clear={this.clearResults}
+                    displayClear={this.state.cards.length !== 0}
+                />
+                <ResultsContainer cards={this.state.cards} phrase={this.state.phrase} />
+            </div>
+        );
     }
-    }
+}
 
-    export default App;
+export default App;
--- client/src/components/results-container/results-container.tsx
+++ client/src/components/results-container/results-container.tsx
@@ -1,22 +1,21 @@
 import { Grid, ImageList, ImageListItem, ListItemText, ListSubheader } from '@mui/material';
-        import { useMediaQuery, useTheme } from "@mui/material";
-        import ManageSearchIcon from '@mui/icons-material/ManageSearch';
-        import { ImageCard, ImageCardProps } from '../image-card/image-card';
+import { useMediaQuery, useTheme } from '@mui/material';
+import ManageSearchIcon from '@mui/icons-material/ManageSearch';
+import { ImageCard, ImageCardProps } from '../image-card/image-card';
 
-        interface ResultsProps {
-        cards: Array
-<ImageCardProps>;
+interface ResultsProps {
+    cards: Array<ImageCardProps>;
     phrase: string;
-    }
+}
 
-    export function ResultsContainer (props: ResultsProps) {
+export function ResultsContainer(props: ResultsProps) {
     const theme = useTheme();
-    const isLarge = useMediaQuery(theme.breakpoints.up("lg"));
-    const isExtraLarge = useMediaQuery(theme.breakpoints.up("xl"));
+    const isLarge = useMediaQuery(theme.breakpoints.up('lg'));
+    const isExtraLarge = useMediaQuery(theme.breakpoints.up('xl'));
 
     if (props.cards.length === 0) {
-    return (
-    <Grid
+        return (
+            <Grid
                 container
                 spacing={0}
                 direction="column"
@@ -32,44 +31,46 @@ import { Grid, ImageList, ImageListItem, ListItemText, ListSubheader } from '@mu
         );
     } else {
         var numCols = 1;
-    
-        if(isExtraLarge) numCols = 3;
-        else if(isLarge) numCols = 2;
+
+        if (isExtraLarge) numCols = 3;
+        else if (isLarge) numCols = 2;
 
         return (
             <Grid
-            container
-            spacing={0}
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-        sx={{
-        minHeight: '100vh',
-        }}
-        >
-<ImageList cols={numCols}>
-<ImageListItem key="Subheader" cols={numCols}>
-<ListSubheader sx={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        }}>
-<h1>Search results for: {props.phrase}</h1>
-        </ListSubheader>
-        </ImageListItem>
-        {props.cards.map((item) => (
-<ImageCard
-key={`${item.imgUrl}-${item.userId}`}
-        title={item.title}
-        imgUrl={item.imgUrl}
-        userName={item.userName}
-        userId={item.userId}
-        postedOn={item.postedOn}
-        score={item.score}
-        />
-        ))}
-        </ImageList>
-        </Grid>
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+                sx={{
+                    minHeight: '100vh',
+                }}
+            >
+                <ImageList cols={numCols}>
+                    <ImageListItem key="Subheader" cols={numCols}>
+                        <ListSubheader
+                            sx={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                            }}
+                        >
+                            <h1>Search results for: {props.phrase}</h1>
+                        </ListSubheader>
+                    </ImageListItem>
+                    {props.cards.map((item) => (
+                        <ImageCard
+                            key={`${item.imgUrl}-${item.userId}`}
+                            title={item.title}
+                            imgUrl={item.imgUrl}
+                            userName={item.userName}
+                            userId={item.userId}
+                            postedOn={item.postedOn}
+                            score={item.score}
+                        />
+                    ))}
+                </ImageList>
+            </Grid>
         );
-        }
-        }
+    }
+}
